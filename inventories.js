4d00e967c31de6cad1b8b5c15e13d05bf3a2fa1f
kryzoid <kryzoid@live.com>
2017-12-09 15:24:08 -0500

    Revert "Shorten bluebird delay" Was throwing 429s
    This reverts commit 261a81951e234c2e80d450ae339d64cfeecae992.

--- inventories.js
+++ inventories.js
@@ -43,7 +43,7 @@ const getInventory = (userId) => {
     private: false
   };
 
-  requestDelay += 5000;
+  requestDelay += 10000;
 
   return bluebird.delay(requestDelay)
     .then(() => request(inventory.url))
